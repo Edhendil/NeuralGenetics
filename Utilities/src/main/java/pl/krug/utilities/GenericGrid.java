28ab81f28be9ecb889ff5ae70bdda1c5856ac419
Edhendil <Edhendil@Edhendil-PC>
2012-03-22 22:04:18 +0100

    Created new module with classes not really tied to any other module.

---
+++ Utilities/src/main/java/pl/krug/utilities/GenericGrid.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.krug.utilities;
+
+/**
+ * Type safe generic two dimensional array.
+ * @author Edhendil
+ */
+public class GenericGrid<T> {
+    
+    private Object[][] grid;
+    private int height;
+    private int width;
+
+    public GenericGrid(int width, int height) {
+        this.height = height;
+        this.width = width;
+        grid = new Object[width][height];
+    }
+
+    public void setElement(int width, int height, T element) {
+        grid[width][height] = element;
+    }
+
+    public T getElement(int width, int height) {
+        return (T) grid[width][height];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    
+}
